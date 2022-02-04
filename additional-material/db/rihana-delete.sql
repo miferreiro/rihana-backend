4d432549f234513a67c215d9607c9d3e7afce793
miferreiro <miguel.ferreiro.diaz@gmail.com>
2022-02-04 09:29:29 +0100

    Adds the missing tables to the database cleanup scripts

--- additional-material/db/rihana-delete.sql
+++ additional-material/db/rihana-delete.sql
@@ -1,12 +1,17 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
+DELETE FROM `action`;
+DELETE FROM `functionality`;
+DELETE FROM `role`;
 DELETE FROM `user`;
+DELETE FROM `functionalityaction`;
+DELETE FROM `permission`;
 DELETE FROM `patient`;
 DELETE FROM `exploration`;
 DELETE FROM `report`;
+DELETE FROM `explorationcode`;
 DELETE FROM `requestedexploration`;
 DELETE FROM `performedexploration`;
-DELETE FROM `explorationcode`;
 DELETE FROM `radiograph`;
 DELETE FROM `signtype`;
 DELETE FROM `sign`;
--- additional-material/db/rihana-drop.sql
+++ additional-material/db/rihana-drop.sql
@@ -1,12 +1,17 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
+DROP TABLE IF EXISTS `action`;
+DROP TABLE IF EXISTS `functionality`;
+DROP TABLE IF EXISTS `role`;
 DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `functionalityaction`;
+DROP TABLE IF EXISTS `permission`;
 DROP TABLE IF EXISTS `patient`;
 DROP TABLE IF EXISTS `exploration`;
 DROP TABLE IF EXISTS `report`;
+DROP TABLE IF EXISTS `explorationcode`;
 DROP TABLE IF EXISTS `requestedexploration`;
 DROP TABLE IF EXISTS `performedexploration`;
-DROP TABLE IF EXISTS `explorationcode`;
 DROP TABLE IF EXISTS `radiograph`;
 DROP TABLE IF EXISTS `signtype`;
 DROP TABLE IF EXISTS `sign`;
